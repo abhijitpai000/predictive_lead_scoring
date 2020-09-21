9895efcd3f2481d0902d9d4aaaea48b2e890a76c
unknown <abhijitpai000@gmail.com>
2020-09-22 00:48:14 +0530

    Add config file

---
+++ src/config.py
@@ -0,0 +1,25 @@
+"""Configuration File"""
+
+TARGET_COLS = 'y'
+
+CAT_COLS = ['job',
+            'marital',
+            'education',
+            'default',
+            'housing',
+            'loan',
+            'contact',
+            'month',
+            'day_of_week',
+            'poutcome']
+
+NUM_COLS = ['age',
+            'duration',
+            'campaign',
+            'pdays',
+            'previous',
+            'emp.var.rate',
+            'cons.price.idx',
+            'cons.conf.idx',
+            'euribor3m',
+            'nr.employed']
